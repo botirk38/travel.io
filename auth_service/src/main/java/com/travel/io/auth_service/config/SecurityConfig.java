fb6c0dcd576aee1328c964967ce9818c28516c37
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-01-28 15:22:26 +0000

    Fix home page issues

--- auth_service/src/main/java/com/travel/io/auth_service/config/SecurityConfig.java
+++ auth_service/src/main/java/com/travel/io/auth_service/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @Configuration
 @EnableWebSecurity
