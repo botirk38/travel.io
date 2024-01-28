5a2122e2e76e6b81a4929e416fb68dfe66b44f14
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-01-28 16:35:31 +0000

    finish user service

---
+++ auth_service/src/main/java/authservice/authservice/controller/UserController.java
@@ -0,0 +1,43 @@
+package authservice.authservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import authservice.authservice.service.UserService;
+import authservice.authservice.model.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User registeredUser = userService.registerUser(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+
+ 
+    
+}
